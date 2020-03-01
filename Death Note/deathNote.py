89327d081a1a9e3d7e7f5345b6d93d5bb1a42a7e
ShomiKhan <nkskl6@gmail.com>
2020-03-01 12:28:35 +0600

    death note updated again

--- Death Note/deathNote.py
+++ Death Note/deathNote.py
@@ -3,8 +3,9 @@ import requests
 from PIL import Image
 import os
 import shutil
+import cv2
 
-url = 'https://manganelo.com/chapter/death_note_colored_edition/chapter_1'
+url = 'https://manganelo.com/chapter/death_note_colored_edition/chapter_2'
 response = requests.get(url)
 if response.ok:
     print('Ok')
@@ -28,19 +29,33 @@ img = []
 pattern = re.compile(r'<img src="(.*?)" alt', re.S)
 pattern1 = re.compile(r'\d+\.jpg')
 imageFilesURL = pattern.findall(response, re.S)
-
+#num = 0
 
 for i in imageFilesURL:
 
     fileName = pattern1.findall(i)
     if len(fileName) == 0:
         continue
-    print(fileName[0], " downloading ... ", i)
+    print(fileName[0], " downloading... ", i)
 
     r = requests.get(i)
     with open(folderName + "/" + fileName[0], "wb") as f:
         f.write(r.content)
-    img.append(Image.open(folderName + "/" + fileName[0]))
+    
+    imageFile = Image.open(folderName + "/" + fileName[0])
+    #img.append(imageFile)
+    
+    if imageFile.size[0] < 1700:
+        img.append(imageFile)
+    else:
+        img.append(imageFile.crop((int(imageFile.size[0]/2), 0, imageFile.size[0]-1, imageFile.size[1]-1)))
+        img.append(imageFile.crop((0, 0, int(imageFile.size[0]/2), imageFile.size[1]-1)))
+    
+    '''
+    if num > 5:
+        break
+    num += 1
+    '''
 
 
 img[0].save(folderName + "/" + folderName + ".pdf", save_all=True, append_images=img[1:])
