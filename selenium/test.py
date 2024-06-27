aff4af917b9feb39e6cc2494af5146ffad25784b
nahK994 <nkskl6@gmail.com>
2024-06-27 21:33:44 +0600

    start learning selenium

---
+++ selenium/test.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+title = driver.title
+print(f"Title = {title}")
+driver.implicitly_wait(0.5)
+
+text_box = driver.find_element(by=By.NAME, value="my-text")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+is_selected = driver.find_element(By.XPATH, value="//input[@id='my-check-1']").is_selected()
+print(f"Checkbox 1 isSelected: {is_selected}")
+is_selected = driver.find_element(By.XPATH, value="//input[@id='my-check-2']").is_selected()
+print(f"Checkbox 2 isSelected: {is_selected}")
+is_enabled = driver.find_element(By.XPATH, value="//input[@name='my-disabled']").is_enabled()
+print(f"Disabled box isEnabled: {is_enabled}")
+
+text_box.send_keys("Selenium")
+submit_button.click()
+message = driver.find_element(by=By.ID, value="message")
+text = message.text
+print(text)
+driver.quit()
