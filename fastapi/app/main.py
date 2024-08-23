cb266a562a98dc2f8b4e86f4d476d1a65523f52c
nahK994 <nkskl6@gmail.com>
2024-08-23 22:46:14 +0600

    create crud using sqlalchemy

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,48 +1,50 @@
 from random import randrange
-from fastapi import FastAPI, HTTPException, status
-from .models import Post
+from fastapi import FastAPI, HTTPException, status, Depends
 from typing import List
+from sqlalchemy.orm import Session
+from .database import engine, get_db
+from . import models, schemas
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-my_posts: List[Post] = []
-def get_post_item(id:int):
-    for i in my_posts:
-        if i.id == id:
-            return i
-    return None
 
+@app.get("/posts", status_code=status.HTTP_200_OK)
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
 
-@app.get("/posts")
-def get_posts():
-    return my_posts
-
-@app.get("/posts/{id}")
-def get_post(id: int):
-    item = get_post_item(id)
+@app.get("/posts/{id}", status_code=status.HTTP_200_OK)
+def get_post(id: int, db: Session = Depends(get_db)):
+    item = db.query(models.Post).filter(models.Post.id == id).first()
     if item is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="not found")
     return item
 
 @app.post("/post", status_code=status.HTTP_201_CREATED)
-def create_post(payload: Post):
-    payload.id = randrange(0,1000)
-    my_posts.append(payload)
-    return payload.model_dump()
+def create_post(payload: schemas.Post, db: Session = Depends(get_db)):
+    post = models.Post(**payload.model_dump())
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    return post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    item = get_post_item(id)
-    if item is None:
+def delete_post(id: int, db: Session = Depends(get_db)):
+    item = db.query(models.Post).filter(models.Post.id == id)
+    if item.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="not found")
-    my_posts.remove(item)
+
+    item.delete(synchronize_session=False)
+    db.commit()
     return
 
 @app.put("/posts/{id}", status_code=status.HTTP_200_OK)
-def update_post(id: int, post: Post):
-    item = get_post_item(id)
-    if item is None:
+def update_post(id: int, post: schemas.Post, db: Session = Depends(get_db)):
+    item = db.query(models.Post).filter(models.Post.id == id)
+    if item.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="not found")
-    item.content = post.content
-    item.title = post.title
-    return item
+    item.update(post.model_dump(), synchronize_session=False)
+    db.commit()
+    return item.first()
--- fastapi/app/models.py
+++ fastapi/app/models.py
@@ -1,8 +1,14 @@
-from typing import Optional
-from pydantic import BaseModel
+from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from .database import Base
 
-class Post(BaseModel):
-    id: Optional[int] = -1
-    title: str
-    content: str
-    published: bool = True
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=True)
+    content = Column(String, nullable=False, server_default='TRUE')
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
